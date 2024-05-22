648c9a99c1ddf23185f5086ff96ef81da6efe67b
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-05-22 15:58:24 -0300

    Updated: variaveis.dart & Created: casting.dart

--- Projeto 1/variaveis/bin/variaveis.dart
+++ Projeto 1/variaveis/bin/variaveis.dart
@@ -1,5 +1,17 @@
-import 'package:variaveis/variaveis.dart' as variaveis;
+// import 'package:variaveis/variaveis.dart' as variaveis; - List<String> arguments - ${variaveis.calculate()}
 
-void main(List<String> arguments) {
-  print('Hello world: ${variaveis.calculate()}!');
+void main() {
+  // Objeto 1 - String
+  String nome = 'Guilherme Tantulli';
+
+  // Objeto 2 - Numerais
+  int idade = 25;
+  double altura = 1.8;
+
+  // Objeto 3 - Boolean
+  bool comparativa = (idade == altura);
+
+  print('Meu nome é $nome, tenho $idade anos de idade e $altura metros de altura.');
+  print(comparativa);
 }
+
