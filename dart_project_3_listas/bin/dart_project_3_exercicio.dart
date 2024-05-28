c11f14107abf7f258750a5a8a71a56a3f8edca27
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-05-28 13:50:08 -0300

    Updated: dart_project_3_exercicio.dart

--- dart_project_3_listas/bin/dart_project_3_exercicio.dart
+++ dart_project_3_listas/bin/dart_project_3_exercicio.dart
@@ -3,6 +3,6 @@ void main() {
   List<dynamic> infoKal = ["Clark", 7.0, "Kent"];
   List<dynamic> infoBruce = [10, "Bruce", "Wayne"];
 
-  double media = (infoDiana[2] + infoKal[1] +  infoBruce[0]) / 2;
+  double media = (infoDiana[2] + infoKal[1] +  infoBruce[0]) / 3;
   print(media);
 }
